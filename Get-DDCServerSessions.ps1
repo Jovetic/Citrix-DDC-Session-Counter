485b19e669309a768779da71bf9ef140e5510581
Jovetic <jovetic@gmail.com>
2018-12-02 20:20:47 +0000

    Update Get-DDCServerSessions.ps1

--- Get-DDCServerSessions.ps1
+++ Get-DDCServerSessions.ps1
@@ -3,48 +3,44 @@
     [cmdletBinding()]
     Param(
         [Parameter(Mandatory=$True,
-                    ValueFromPipeLine=$True,
-                    ValueFromPipeLineByPropertyName=$True,
-                    HelpMessage="The Citrix Delivery Controller you want to Query.  Accepts Multiple Servers")]
+                   ValueFromPipeLine=$True,
+                   ValueFromPipeLineByPropertyName=$True,
+                   HelpMessage="The Citrix Delivery Controller you want to Query.  Accepts Multiple Servers.")]
         [Alias('Hostname','cn','name')]                 
         [String[]]$Computername)
 
-
-
     BEGIN {}
     PROCESS {
-    
-
-        ForEach ($Computer in $ComputerName){
-            Try{
-        
-            $Session = New-PSSession -ComputerName $Computer -ErrorAction Stop
-            $Cou = Invoke-Command -Session $Session {Add-PSSnapin Citrix*;Get-BrokerSession}
-            $Act = $Cou|Where-Object{$_.SessionState -eq 'Active'}
-            $Dis = $Cou|Where-Object{$_.SessionState -eq 'Disconnected'}
-            $Old = $Dis|Where-Object{$_.StartTime -lt (Get-Date).AddDays(-10)}
+            ForEach ($Computer in $ComputerName){
+                Try{
             
-            $Properties =[ordered] @{Computer     = $Computer
-                                     Sessions     = $Cou.Count
-                                     Active       = $Act.Count
-                                     Disconnected = $Dis.count
-                                     Stale        = $Old.count}
-            }Catch{
-            Write-Verbose "Couldn't connect to $Session"
-            $Properties =[ordered] @{Computer     = $Computer
-                                     Sessions     = $null
-                                     Active       = $null
-                                     Disconnected = $null
-                                     Stale        = $null
-                                     Status       = 'Not available'}
+                    $Session = New-PSSession -ComputerName $Computer -ErrorAction Stop
+                    $Cou = Invoke-Command -Session $Session {Add-PSSnapin Citrix*;Get-BrokerSession}
+                    $Act = $Cou|Where-Object{$_.SessionState -eq 'Active'}
+                    $Dis = $Cou|Where-Object{$_.SessionState -eq 'Disconnected'}
+                    $Old = $Dis|Where-Object{$_.StartTime -lt (Get-Date).AddDays(-10)}
+                    
+                    $Properties =[ordered] @{Computer    = $Computer
+                                            Sessions     = $Cou.Count
+                                            Active       = $Act.Count
+                                            Disconnected = $Dis.Count
+                                            Stale        = $Old.Count}
+                }Catch{
+                    Write-Verbose "Couldn't connect to $Session"
+                    $Properties =[ordered] @{Computer    = $Computer
+                                            Sessions     = $null
+                                            Active       = $null
+                                            Disconnected = $null
+                                            Stale        = $null
+                                            Status       = 'Not available'}
 
-            }finally {       
-            $AllSessions = New-Object -TypeName PSObject -Property $Properties
-            $AllSessions.psobject.typenames.insert(0,'Citrix.Sessions.Object')
-            Write-Output $AllSessions
-                 }   
+                }Finally {       
+                    $AllSessions = New-Object -TypeName PSObject -Property $Properties
+                    $AllSessions.psobject.typenames.insert(0,'Citrix.Sessions.Object')
+                    Write-Output $AllSessions
+                }   
+            }
         }
-     }
     END {}
    }
 
