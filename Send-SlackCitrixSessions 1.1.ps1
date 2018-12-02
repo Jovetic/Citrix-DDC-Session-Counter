0914e157ae5e0eb03fc87ca871e16e02999bfb8d
Jovetic <jovetic@gmail.com>
2018-12-02 20:25:20 +0000

    Create Send-SlackCitrixSessions 1.1.ps1
    Adding custom function for formatting message sent to Slack.

---
+++ Send-SlackCitrixSessions 1.1.ps1
@@ -0,0 +1,22 @@
+﻿Function Send-SlackCitrixSessions{
+
+$Data = Get-DDCServerSessions
+
+$SlackText = @()
+        ForEach ($line in $Data){
+Try{
+        $A=$Line.Sessions
+        $B=$Line.Computer
+        $C=$Line.Active
+        $D=$Line.Disconnected
+        $E=$Line.Stale
+}Catch{
+}Finally{
+        $SlackText += "*$A* Sessions on *$B*, *$C* Active, *$D* Disconnected. *$E* of which have been disconnected more than *10* days"
+}
+}
+Write-Output $SlackText
+}
+
+
+
