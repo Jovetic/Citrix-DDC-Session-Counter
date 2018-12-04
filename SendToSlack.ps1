fa7d4607201f0e1ddfa38cdd6fcc3f700628da08
Jovetic <jovetic@gmail.com>
2018-12-04 18:15:32 +0000

    Update SendToSlack.ps1

--- SendToSlack.ps1
+++ SendToSlack.ps1
@@ -1,6 +1,5 @@
 ﻿Send-SlackMessage -Text (Get-DDCServerSessions -Computername "SERVER","SERVER01"|Out-String) -Webhook "https://hooks.slack.com/services/YOURWEBHOOKURI"
 
 Send-SlackMessage -Text ((Get-Content C:\scripts\Citrix\Private\Computers.txt)|Get-DDCServerSessions|Out-string) -Webhook "https://hooks.slack.com/services/YOURWEBHOOKURI"
-Send-SlackMessage -Text ((Get-Content C:\scripts\Citrix\Private\Computers.txt)|Get-DDCServerSessions|Out-string) -Webhook "https://hooks.slack.com/services/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"
 
 Send-SlackMessage -Text (Send-SlackCitrixSessions|Out-String) -Webhook "https://hooks.slack.com/services/YOURWEBHOOKURI"
